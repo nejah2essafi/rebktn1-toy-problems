46ad2828d8a0c72560b832b4bd54159723f2e0e7
nejah2essafi <nejah.essafi@hotmail.fr>
2019-11-25 09:16:58 +0100

    toydone

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -7,5 +7,24 @@
  */
 
 var firstNonRepeatedCharacter = function(string) {
+	var array= string.split("")
+	var obj={}
+	for(var i =0; i<array.length; i++){
+		if(obj[array[i]]===undefined){
+			obj[array[i]]=1
+		}else {
+			obj[array[i]]++
+		}
+	}
+	for(var j=0;j<array.length;j++){
+		for(var key in obj){
+			if(obj[array[j]]!==1){
+				array.splice(j,1)
+			}
+		}
+	}
+	return array[0]
+
+
   // TODO: your solution here
 };
